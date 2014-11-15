3c641be28c0c0f8b4ef98d68a1be0bd9ba7c2c42
Melvin <Melvine@Melvins-MacBook-Pro.local>
2014-11-15 03:44:05 -0800

    app changes

--- cat year app/ViewController.swift
+++ cat year app/ViewController.swift
@@ -12,9 +12,12 @@ class ViewController: UIViewController {
 
     
     @IBOutlet weak var humanAge: UITextField!
+    @IBOutlet weak var resultLabel: UILabel!
     
     @IBAction func convertAge(sender: AnyObject) {
         println(humanAge.text)
+        var x = humanAge.text.toInt()! * 7
+        resultLabel.text = String(x)
     }
     
     
